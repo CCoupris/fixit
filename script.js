7760a292d214d410b496c572b820ba49484d4b16
Azenstrange <edouard.eisenfisz@epita.fr>
2019-12-13 03:31:03 +0100

    :lipstick: Simplistic look

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var Quagga = window.Quagga;
 var App = {
     _scanner: null,
     init: function() {
-        this.attachListeners();
+        this.attachListeners()
     },
     decode: function(file) {
         Quagga
@@ -11,33 +11,33 @@ var App = {
             .fromSource(file, {size: 800})
             .toPromise()
             .then(function(result) {
-                document.querySelector('input.isbn').value = result.codeResult.code;
+                document.querySelector('input.isbn').value = result.codeResult.code
             })
             .catch(function() {
-                document.querySelector('input.isbn').value = "Not Found";
+                document.querySelector('input.isbn').value = "Not Found"
             })
             .then(function() {
-                this.attachListeners();
-            }.bind(this));
+                this.attachListeners()
+            }.bind(this))
     },
     attachListeners: function() {
         var self = this,
             button = document.querySelector('.input-field input + .button.scan'),
-            fileInput = document.querySelector('.input-field input[type=file]');
+            fileInput = document.querySelector('.input-field input[type=file]')
 
         button.addEventListener("click", function onClick(e) {
-            e.preventDefault();
-            button.removeEventListener("click", onClick);
-            document.querySelector('.input-field input[type=file]').click();
-        });
+            e.preventDefault()
+            button.removeEventListener("click", onClick)
+            document.querySelector('.input-field input[type=file]').click()
+        })
 
         fileInput.addEventListener("change", function onChange(e) {
-            e.preventDefault();
-            fileInput.removeEventListener("change", onChange);
+            e.preventDefault()
+            fileInput.removeEventListener("change", onChange)
             if (e.target.files && e.target.files.length) {
-                self.decode(e.target.files[0]);
+                self.decode(e.target.files[0])
             }
-        });
+        })
     }
-};
-App.init();
+}
+App.init()
