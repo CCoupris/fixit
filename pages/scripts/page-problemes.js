2cbda87758a546a4f1fea2767b6d8e24b52f99ec
CatincaG <47737587+CatincaG@users.noreply.github.com>
2019-12-16 11:10:06 +0100

    :lipstick: progress bar in progress

--- pages/scripts/page-problemes.js
+++ pages/scripts/page-problemes.js
@@ -2,6 +2,49 @@ const bateryButton = document.querySelector('.js-batery-button')
 const continueButton = document.querySelector('.js-continue-button')
 const bateryIcon = document.querySelector('.js-batery-icon')
 
+// Progress bar variables
+let step = 'step1'
+
+const step1 = document.getElementById('step1')
+const step2 = document.getElementById('step2')
+const step3 = document.getElementById('step3')
+const step4 = document.getElementById('step4')
+
+// Making the PROGRESS STEP BAR working
+function next() {
+  if (step === 'step1') {
+    step = 'step2';
+    step1.classList.remove("is-active");
+    step1.classList.add("is-complete");
+    step2.classList.add("is-active");
+
+  } else if (step === 'step2') {
+    step = 'step3';
+    step2.classList.remove("is-active");
+    step2.classList.add("is-complete");
+    step3.classList.add("is-active");
+
+  } else if (step === 'step3') {
+    step = 'step4d';
+    step3.classList.remove("is-active");
+    step3.classList.add("is-complete");
+    step4.classList.add("is-active");
+
+  } else if (step === 'step4d') {
+    step = 'complete';
+    step4.classList.remove("is-active");
+    step4.classList.add("is-complete");
+
+  } else if (step === 'complete') {
+    step = 'step1';
+    step4.classList.remove("is-complete");
+    step3.classList.remove("is-complete");
+    step2.classList.remove("is-complete");
+    step1.classList.remove("is-complete");
+    step1.classList.add("is-active");
+  }
+}
+
 // Button selection
 bateryButton.addEventListener('click', () =>
 {
