c49e880a97b8d55edc5ba6a133fad21b18e08a1e
CatincaG <47737587+CatincaG@users.noreply.github.com>
2019-12-18 16:31:49 +0100

    :sparkles: holder info button hided

--- pages/scripts/confirmation.js
+++ pages/scripts/confirmation.js
@@ -21,10 +21,37 @@ class Open {
     }
   }
 
-let list = document.querySelectorAll("li")
+let list = document.querySelectorAll("h4")
 let faq = new Open()
 for (let i = 0; i < list.length; i++) {
   list[i].addEventListener("click", function() {
     faq.open(i,list)
   })
 }
+
+
+// CARD FORM 
+const cardHolder = document.querySelector('.js-card-holder')
+const cardInformation = document.querySelector('.js-card-information')
+const holderDisplay = document.querySelector('.card-holder-display')
+const validateButton = document.querySelector('.js-button-valid')
+
+    //
+function toggleColor() {
+  if (cardInformation.style.backgroundColor == "white") {
+    cardInformation.style.backgroundColor = "#B42F3C";
+  } else {
+    cardInformation.style.backgroundColor = "white";
+  }
+}
+    toggleColor()
+cardInformation.addEventListener('click', () =>
+{
+    toggleColor()
+    validateButton.style.backgroundColor = "#B42F3C"
+})
+
+validateButton.addEventListener("click", () =>
+{
+  cardHolder.classList.toggle('card-holder-display')
+})
